7d5a19fbf8d0b60ad2eda8eb4037b9c56030f8d3
Cameron Probert <c.probert@hotmail.co.nz>
2018-03-24 17:09:51 +1300

    started testing pug

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
+const pug = require('pug')
 
 const server = express()
 
 server.use(express.static('public'))
 server.use(bodyParser.urlencoded({ extended: true }))
+server.set('view engine', 'pug')
 
-server.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'))
+//const blogTemplate = pug.renderFile('template-blog.pug')
+
+/* server.get('/test', (req, res) => {
+  res.send(pug.renderFile('./views/template-blog.pug'))
+}) */
+
+server.get('/test', (req, res) => {
+  res.render('template-blog')
 })
 
 module.exports = server
